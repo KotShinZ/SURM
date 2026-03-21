c667aa80ec8fbce1d65ffda7cf6bde922ce43337
KotShinZ <s18574s18574@gmail.com>
2026-03-22 03:10:34 +0900

    update for overlearning.

--- scripts/URM_sudoku_H100.sh
+++ scripts/URM_sudoku_H100.sh
@@ -3,13 +3,13 @@ checkpoint_path="checkpoints/${run_name}"
 mkdir -p $checkpoint_path
 
 torchrun --nproc-per-node 1 pretrain.py \
-data_path=data/sudoku-extreme-1k-aug-1000 \
+data_path=data/sudoku-extreme-1k-aug-all \
 arch=urm arch.loops=16 arch.H_cycles=1 arch.L_cycles=6 arch.num_layers=4 \
 arch.grid_height=0 arch.grid_width=0 \
 evaluators="[]" \
-epochs=50000 \
+epochs=10 \
 data_fraction=1 \
-eval_interval=5000 \
+eval_interval=1 \
 lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=768 \
 +run_name=$run_name \
 +checkpoint_path=$checkpoint_path \
