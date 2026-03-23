c2cb5fadcfa40678a6722d305956df4f2504403e
KotShinZ <s18574s18574@gmail.com>
2026-03-23 22:42:42 +0900

    data prepare fix.

--- data/build_sudoku_dataset.py
+++ data/build_sudoku_dataset.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 from tqdm import tqdm
 from huggingface_hub import hf_hub_download
 
-from common import PuzzleDatasetMetadata
+from data.common import PuzzleDatasetMetadata
 
 
 cli = ArgParser()
