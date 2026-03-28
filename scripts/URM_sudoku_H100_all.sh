2d1a08ad0bec33f538fe506e0481449c16735f6d
KotShinZ <s18574s18574@gmail.com>
2026-03-28 17:15:30 +0900

    target-learning is no good.

--- scripts/URM_sudoku_H100_all.sh
+++ scripts/URM_sudoku_H100_all.sh
@@ -3,13 +3,13 @@ checkpoint_path="checkpoints/${run_name}"
 mkdir -p $checkpoint_path
 
 torchrun --nproc-per-node 1 pretrain.py \
-data_path=data/sudoku-extreme-1k-aug-1000 \
-arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=24 arch.num_layers=1 \
+data_path=data/sudoku-extreme-1k-aug-all \
+arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=6 arch.num_layers=4 \
 arch.grid_height=9 arch.grid_width=9 \
 evaluators="[]" \
-epochs=50000 \
+epochs=15 \
 data_fraction=1 \
-eval_interval=5000 \
+eval_interval=1 \
 target_mask_mode=all \
 lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=768 \
 +run_name=$run_name \
