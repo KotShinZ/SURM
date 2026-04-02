628637e32485033806e69e5fa98082021f10eeb8
KotShinZ <s18574s18574@gmail.com>
2026-04-03 03:43:20 +0900

    this is arcagi base.

--- scripts/URM_arcagi1_H100.sh
+++ scripts/URM_arcagi1_H100.sh
@@ -5,14 +5,15 @@ mkdir -p $checkpoint_path
 torchrun --nproc-per-node 1 pretrain.py \
 data_path=data/arc1concept-aug-1000 \
 arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=6 arch.num_layers=4 \
+arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
 arch.grid_height=30 arch.grid_width=30 \
 global_batch_size=128 \
-evaluators="[]" \
-grad_accum_steps=1 \
+grad_accum_steps=6 \
 epochs=200000 \
-eval_interval=20000 \
+eval_interval=2000 \
 puzzle_emb_lr=1e-4 \
 weight_decay=0.1 \
 +run_name=$run_name \
 +checkpoint_path=$checkpoint_path \
-+ema=True
++ema=True \
+evaluators="[]"
