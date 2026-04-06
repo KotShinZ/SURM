c754c8acc2a77086d189f86e68b59133831946d9
KotShinZ <s18574s18574@gmail.com>
2026-04-06 21:47:16 +0900

    fix bash.

--- scripts/URM_maze_H100.sh
+++ scripts/URM_maze_H100.sh
@@ -8,9 +8,9 @@ arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=6 arch.num_layers=4 \
 arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
 arch.grid_height=30 arch.grid_width=30 \
 evaluators="[]" \
-epochs=10000 \
+epochs=5000 \
 data_fraction=1 \
-eval_interval=1000 \
+eval_interval=200 \
 lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=128 \
 +run_name=$run_name \
 +checkpoint_path=$checkpoint_path \
