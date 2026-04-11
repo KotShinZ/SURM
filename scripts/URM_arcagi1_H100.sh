9fcde64373b227e4b4d5afb83b24b8e7d090faff
KotShinZ <s18574s18574@gmail.com>
2026-04-11 12:03:18 +0900

    fix command.

--- scripts/URM_arcagi1_H100.sh
+++ scripts/URM_arcagi1_H100.sh
@@ -7,11 +7,11 @@ data_path=data/arc1concept-aug-1000-unpadded \
 arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=1 arch.num_layers=4 \
 arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
 arch.grid_height=30 arch.grid_width=30 \
-global_batch_size=128 \
-grad_accum_steps=6 \
+global_batch_size=768 \
+grad_accum_steps=1 \
 epochs=200000 \
 eval_interval=2000 \
-puzzle_emb_lr=1e-4 \
+puzzle_emb_lr=1e-2 \
 weight_decay=0.1 \
 +run_name=$run_name \
 +checkpoint_path=$checkpoint_path \
