7fb28ff4879b4970434622096f25e31b7ab42b62
KotShinZ <s18574s18574@gmail.com>
2026-05-22 15:51:01 +0900

    use HL

---
+++ scripts/URM_arcagi1_H100_gen_HL.sh
@@ -0,0 +1,20 @@
+run_name="URM-arcagi1-gen_HL"
+checkpoint_path="checkpoints/${run_name}" 
+mkdir -p $checkpoint_path
+
+torchrun --nproc-per-node 1 pretrain.py \
+data_path=data/arc1withgen-aug-1000 \
+arch=urm arch.loops=32 arch.H_cycles=2 arch.L_cycles=3 arch.H_layers=1 arch.L_layers=3 arch.grad_H_cycles=2 \
+arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
+arch.grid_height=30 arch.grid_width=30 \
+global_batch_size=128 \
+grad_accum_steps=6 \
+epochs=200000 \
+eval_interval=2000 \
+torch_compile=True \
+puzzle_emb_lr=1e-2 \
+weight_decay=0.1 \
++run_name=$run_name \
++checkpoint_path=$checkpoint_path \
++ema=True \
+evaluators="[]"
