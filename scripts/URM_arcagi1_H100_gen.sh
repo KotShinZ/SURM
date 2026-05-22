9b87bd8f6575d205f7a945e635a6430ca4ea8623
KotShinZ <s18574s18574@gmail.com>
2026-05-23 05:21:51 +0900

    fix.

--- scripts/URM_arcagi1_H100_gen.sh
+++ scripts/URM_arcagi1_H100_gen.sh
@@ -1,11 +1,11 @@
-run_name="URM-arcagi1-gen"
+run_name="URM-arcagi1-gen-act-l8"
 checkpoint_path="checkpoints/${run_name}" 
 mkdir -p $checkpoint_path
 
 torchrun --nproc-per-node 1 pretrain.py \
-data_path=data/arc1gen-nopadding \
-arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=1 arch.num_layers=4 \
-arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
+data_path=data/arc1withgen-aug-1000 \
+arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=1 arch.num_layers=8 \
+arch.use_act=True arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
 arch.grid_height=30 arch.grid_width=30 \
 global_batch_size=768 \
 grad_accum_steps=1 \
