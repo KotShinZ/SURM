bf4a7a34bafc03df1b6cfb0c7118ea082c730564
KotShinZ <s18574s18574@gmail.com>
2026-06-08 18:53:55 +0900

    defaut training config.

--- scripts/URM_arcagi1_H100_full_gen.sh
+++ scripts/URM_arcagi1_H100_full_gen.sh
@@ -5,8 +5,8 @@ mkdir -p $checkpoint_path
 
 torchrun --nproc-per-node 1 pretrain.py \
 data_path=data/arc1withgen-aug-1000 \
-arch=urm arch.loops=8 arch.H_cycles=1 arch.L_cycles=1 arch.num_layers=4 arch.hidden_size=512 \
-arch.forward_mode=casual arch.answer_only_context_layers=0 arch.input_injection_enabled=True \
+arch=urm arch.loops=32 arch.H_cycles=1 arch.L_cycles=1 arch.num_layers=4 arch.hidden_size=512 \
+arch.forward_mode=standard arch.answer_only_context_layers=0 arch.input_injection_enabled=True \
 arch.use_act=False arch.norm_diff_max=0.1 arch.norm_diff_min=0.001 \
 arch.loss.label_mask=0.0 \
 +arch.num_memory_tokens=0 \
@@ -24,7 +24,7 @@ weight_decay=0.1 \
 +ema=True \
 evaluators="[]" \
 +mask_full_training=True \
-+padding=False \
++padding=True \
 full_answer_initial_mode="noised_label" \
 use_muon=False \
 # --load_checkpoint_file checkpoints/URM-nca2d/step_13020.pt
